ed9475ce6d8a61be9e299c55f6e024cd8d53baab
Shreyansh Chaudhary <shreyanshchaudhary661@gmail.com>
2026-05-25 17:48:29 +0530

    Add pw-skills strings
    Add examples of string operations including concatenation, slicing, and escape characters.

---
+++ pw-skills/strings.py
@@ -0,0 +1,18 @@
+# String - group of sequential characters
+# immutable hai
+
+x = '1'
+y = '2'
+concatenation = x + y   # string join
+print(len(concatenation))  # length = 2
+
+# slicing
+str = 'Shreyansh'
+print(str[2:5])   # ney
+
+# escape characters
+# \n = new line
+# \\ = backslash
+# \' = single quote
+print("Hello\nShreyansh")   # new line pe print
+print("He said \"Hello\"")  # quotes inside string
